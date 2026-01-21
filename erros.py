0d57d843151e4e439ee04b74b51cb4d99358ca54
Pedro Ainsworth Ramos Luizeto <pedroluizeto631@gmail.com>
2026-01-21 13:26:02 -0300

    Refactor error handling in erros.py

--- erros.py
+++ erros.py
@@ -1,11 +1,3 @@
-#try:
-   # n1 = int(input("Digite o primeiro número: "))
-    #n2 = int(input("Digite o segundo número: "))
-    #resultado = n1 + n2
-    #print(f"O resultado da soma é: {resultado}")
-#except ValueError as error:
-#    print(f"Ocorreu um erro ao tentar somar os números: {error}")
-
 try:
     numero = float(input("Digite um número : "))
     resultado = 10 / numero
@@ -15,4 +7,5 @@ except ZeroDivisionError:
     print("Erro: Divisão por zero não é permitida.")
 
 except ValueError:
+
     print("Erro: Entrada inválida. Por favor, digite um número válido.")
